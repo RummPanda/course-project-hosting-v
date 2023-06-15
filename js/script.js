f08a1f88c0c081a979604e0358d9b731c86866fe
RummPanda <rumm.panda@gmail.com>
2023-06-16 01:10:17 +0300

    -

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 
+
 const loader = new THREE.GLTFLoader();
 
 loader.load(
